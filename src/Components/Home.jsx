560e42ece862b6222bf2b2716e5469820b7b319c
DvnCodes <DvnCodes@users.noreply.github.com>
2020-03-09 20:09:15 +0000

    add Loading indication to homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,18 +3,26 @@ import ArticleList from "./Articles/ArticleList";
 import * as api from "../api";
 
 class Home extends Component {
-  state = { newest3: [], top3: [] };
+  state = { newest3: [], top3: [], isLoading: true };
   render() {
     return (
       <div className="home">
         <h1>Home</h1>
         <h2 className="newest3">Newest Articles</h2>
         <div className="homelist">
-          <ArticleList articles={this.state.newest3} />
+          {!this.state.isLoading ? (
+            <ArticleList articles={this.state.newest3} />
+          ) : (
+            <h3>Loading...</h3>
+          )}
         </div>
         <h2>Top Rated</h2>
         <div className="homelist">
-          <ArticleList className="homelist" articles={this.state.top3} />
+          {!this.state.isLoading ? (
+            <ArticleList className="homelist" articles={this.state.top3} />
+          ) : (
+            <h3>Loading...</h3>
+          )}
         </div>
       </div>
     );
@@ -25,7 +33,7 @@ class Home extends Component {
       this.setState({ newest3: res });
     });
     api.fetchArticles(undefined, "votes", 3).then(res => {
-      this.setState({ top3: res });
+      this.setState({ top3: res, isLoading: false });
     });
   }
 }
